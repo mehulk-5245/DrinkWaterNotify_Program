5e34a2dfa242b5f4fef30ac02c1bbbb6ee524ff1
Mehul Khandhadiya <53016215+mehulk-5245@users.noreply.github.com>
2023-07-11 19:14:56 +0530

    Update compiletime.py

--- compiletime.py
+++ compiletime.py
@@ -1,7 +1,6 @@
-a = [1, 12, 23, 44, 96, 102]
-key = 44
-for i in range (len(a)):
-  if(a[i] == key):
-    print("index is "+ i)
-  else:
-    print("not found")
+a = 2
+b = 3
+sum = a + b
+d = 41
+output = sum + d
+print(output)
